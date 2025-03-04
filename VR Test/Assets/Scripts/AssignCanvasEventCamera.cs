9287552d151a63b252bdb2f2abe8c3c325c60d74
Donald Paladino <106718081+DUBBEP@users.noreply.github.com>
2025-03-04 16:07:05 -0700

    UI Dev Started

---
+++ VR Test/Assets/Scripts/AssignCanvasEventCamera.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+public class AssignCanvasEventCamera : MonoBehaviour
+{
+    void Start()
+    {
+        Canvas canvas = GetComponent<Canvas>();
+        if (canvas && canvas.worldCamera == null)
+        {
+            canvas.worldCamera = Camera.main;
+        }
+    }
+}
